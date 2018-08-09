b683632c224ace53dadddbc37d95057aa0119df8
wendy <3063766545@qq.com>
2018-08-09 18:18:44 +0800

    add ico

--- src/page/top250.vue
+++ src/page/top250.vue
@@ -1,9 +1,27 @@
 <script>
-    
+import { Tab, TabItem } from 'vux'
+export default {
+    components: {
+        Tab,
+        TabItem,
+    },
+    data(){
+        return {}
+    },
+    methods:{
+        onItemClick (index) {
+            console.log('on item click:', index)
+        },
+    },
+}
 </script>
 <template>
     <div>
-        <h1>this is top250 page</h1>
+        <tab>
+            <tab-item selected @on-item-click="onItemClick">已发货</tab-item>
+            <tab-item @on-item-click="onItemClick">未发货</tab-item>
+            <tab-item @on-item-click="onItemClick">全部订单</tab-item>
+        </tab>
     </div>
 </template>
 <style>
