5ec4d817f196931ce44a80c70c1b8d379af0db22
wendy <3063766545@qq.com>
2018-08-20 10:56:32 +0800

    add top250 and some methods

--- src/components/topItem.vue
+++ src/components/topItem.vue
@@ -1,12 +1,197 @@
 <script>
-    
+import Rate from './rate'
+import { Flexbox, FlexboxItem} from 'vux';
+
+export default {
+    props: ['film', 'num', 'start'],
+    data () {
+        return {
+            director: '',
+            cast: '',
+            type: ''
+        }
+    },
+    components: {
+        Rate,
+        Flexbox,
+        FlexboxItem,
+    },
+    mounted () {
+        this.director = this.film.directors.map((value) => {
+            return value.name
+        }).join(' / ');
+        this.cast = this.film.casts.map((value) => {
+            return value.name
+        }).join(' / ');
+        this.type = this.film.genres.join(' / ');
+    },
+    methods: {
+        clickItem: function () {
+            this.$router.push({ path: '/filmInfo', query: { id: this.film.id } });
+            this.$store.commit('updateBackStatus', {isShowBack: true});
+        }
+    }
+}
 </script>
+
 <template>
-    <div>
-        this is topitem page
+    <div class="top-item" @click="clickItem">
+        <!-- <div class="left">
+            <div class="title-year">
+                <span class="rank">No.{{start + num + 1}}</span>
+                <span class="title">{{film.title}}</span>
+                <span class="year">{{film.year}}</span>
+            </div>
+            <div class="rate">
+                <Rate class="rating" :score="film.rating.average"></Rate>
+                <span class="num">({{film.collect_count}}人评价)</span>
+            </div>
+            <p class="director">导演 {{director}}</p>
+            <p class="cast">主演 {{cast}}</p>
+            <p class="type">类型 {{type}}</p>
+        </div>
+        <div class="right">
+        <img class="image" :src="film.images.medium" >
+        </div> -->
+
+        <flexbox>
+            <flexbox-item>
+                <div class="title-year">
+                    <span class="rank">No.{{start + num + 1}}</span>
+                    <span class="title">{{film.title}}</span>
+                    <span class="year">{{film.year}}</span>
+                </div>
+                <div class="rate">
+                    <Rate :score="film.rating.average"></Rate>
+                    <span class="num">({{film.collect_count}}人评价)</span>
+                </div>
+                <p class="director">导演 {{director}}</p>
+                <p class="cast">主演 {{cast}}</p>
+            </flexbox-item>
+            <flexbox-item :span="4">
+                <img class="image" :src="film.images.medium" >
+            </flexbox-item>
+        </flexbox>
     </div>
 </template>
-<style>
 
+<style lang="less">
+// .top-item {
+//   overflow: hidden;
+//   margin-bottom: 1rem;
+//   border: 0.02rem solid #DDD;
+//   border-radius: 0.3em;
+//   width: 100%;
+//   background-color: #FFF;
+//   padding: 0.5rem;
+//   box-sizing: border-box;
+  
+//   .left {
+//     font-size: 0.9rem;
+//     float: left;
+//     color: #222;
+//     width: 15rem;
+//     margin-right: 0.8rem;
+//     .title-year {
+//       .rank {
+//         display: inline-block;
+//         box-sizing: border-box;
+//         padding: 0.5rem;
+//         background-color: #008040;
+//         color: #FFF;
+//         height: 100%;
+//         font-weight: bold;
+//         border-radius: 0.3em;
+//         font-size: 1rem;
+//       }
+//       .title {
+//         font-size: 1.05rem;
+//       }
+//       .year {
+//         font-size: 0.9rem;
+//         color: #666;
+//       }
+//     }
+//     .num {
+//       font-size: 0.8rem;
+//       color: #666;
+//     }
+    
+//   }
+//   .right {
+//     float: right;
+//     width: 7.5rem;
+//     height: 10.5rem;
+
+//     .image {
+//       width: 7.5rem;
+//       height: 10.5rem;
+//       border-radius: 0.3em;
+//     }
+//   }
+// }
+// .director, .cast, .type {
+//   width: 100%;
+//   overflow: hidden;
+//   text-overflow: ellipsis;
+//   white-space: nowrap;
+// }
+
+// new
+.top-item{
+    overflow: hidden;
+    margin-bottom: 1rem;
+    border: 0.02rem solid #DDD;
+    border-radius: 0.3em;
+    width: 100%;
+    background-color: #FFF;
+    padding: 0.5rem;
+    box-sizing: border-box;
+}
+.film_box{
+    font-size: 1rem;
+    overflow: hidden;
+    margin-bottom: 1rem;
+    border: 0.02rem solid #DDD;
+    border-radius: 0.3em;
+    width: 100%;
+    background-color: #FFF;
+}
+.image {
+    width: 7.5rem;
+    height: 10.5rem;
+    line-height: 10.5rem;
+    border-radius: 0.3em;
+}
+.title-year {
+    margin: 0.5rem 0;
+    .rank {
+        display: inline-block;
+        box-sizing: border-box;
+        padding: 0.5rem;
+        background-color: rgb(64, 158, 255);
+        color: #FFF;
+        height: 100%;
+        font-weight: bold;
+        border-radius: 0.3em;
+        font-size: 1rem;
+    }
+    .title {
+        font-size: 1.05rem;
+    }
+    .year {
+        font-size: 0.9rem;
+        color: #666;
+    }
+}
+.director {
+    margin-top: 0.8rem;
+}
+
+.director, .cast {
+    width: 100%; 
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap; 
+}
 </style>
-
--- src/page/top250.vue
+++ src/page/top250.vue
@@ -1,10 +1,12 @@
 <script>
+import TopItem from '../components/topItem.vue';
 import formatTime from '../utils/formatTime.js';
 import { mapState } from 'vuex';
 import { AjaxPlugin, Tab, TabItem, Loading, Divider } from 'vux';
 
 export default {
     components: {
+        TopItem,
         Tab,
         TabItem,
         Loading,
@@ -20,55 +22,87 @@ export default {
             films: [],
             start: 0,
             max: 50,
-            count: 50
+            count: 25
         }
+    },
+    computed: {
+        ...mapState({
+            isLoading: state =>state.isLoading
+        })
     },
     methods:{
         getInitData(){
-            this.$store.commit('updateLoadingStatus', {isLoading: true});
-            let isLoad = true;
-            let oldDate = '';
-            let data = localStorage.getItem('top250');
-            let date = formatTime.toDate(new Date(), '/');
-            
-            if(data){
-                oldDate = data.substring(0, 10);
-                data = JSON.parse(data.substring(11));
-            }
-            if(data && oldDate == date){
-                isLoad = false;
-            }
-            if(isLoad){
-                let _this = this;
-                AjaxPlugin.$http.get(this.HOME+'/movie/top250?'+ 'start=' + start + '&count=' + count).then((res) => {
-                    let result = date+'='+ JSON.stringify(res.data.subjects);
-                    localStorage.setItem('top250', result);
-                    _this.films = res.data.subjects;
-                    _this.$store.commit('updateLoadingStatus', {isLoading: false});
-                });
-            }else{
-                this.films = data;
-                console.log(data);
-                this.$store.commit('updateLoadingStatus', {isLoading: false});
-            }
+            console.log(this.count);
+            var _this = this
+            AjaxPlugin.$http.get('https://api.douban.com/v2/movie/top250?' + 'start=' + this.start + '&count=' + this.count).then((res) => {
+                _this.films = res.data.subjects
+                _this.$store.commit('updateLoadingStatus', {isLoading: false})
+            })
         },
         onItemClick (index) {
-            console.log('on item click:', index)
+            console.log('on item click:', index);
+            this.films = [];
+            this.$store.commit('updateLoadingStatus', {isLoading: true});
+            this.count = 25;
+            switch (index) {
+                case 0:
+                    this.start = 0
+                    this.max = 50
+                    break
+                case 1:
+                    this.start = 50
+                    this.max = 50
+                    break
+                case 2:
+                    this.start = 100
+                    this.max = 50
+                    break
+                case 3:
+                    this.start = 150
+                    this.max = 50
+                    break
+                case 4:
+                    this.start = 200
+                    this.max = 50
+                    break
+            }
+            this.getInitData();
+        },
+        loadMore: function () {
+            var mbody = document.documentElement || document.body;
+            var mScrollTop = mbody.scrollTop;
+            var mClientHeight = mbody.clientHeight;
+            var mScrollHeight = mbody.scrollHeight;
+            // console.log(mScrollTop, mClientHeight, mScrollHeight)
+            // console.log(mScrollTop + mClientHeight === mScrollHeight)
+            if (this.count < this.max && mScrollTop + mClientHeight === mScrollHeight) {
+                this.$store.commit('updateLoadingStatus', {isLoading: true});
+                this.count += 25;
+                console.log(this.count, this.max);
+                this.getInitData();
+            }
         },
     },
     mounted(){
-        // this.getInitData();
+        this.$store.commit('updateLoadingStatus', {isLoading: true});
+        this.getInitData();
     },
+    created: function () {
+        window.addEventListener('scroll', this.loadMore, false);
+    },
+    destroyed: function () {
+        window.removeEventListener('scroll', this.loadMore, false);
+    }
 }
 </script>
 <template>
     <div>
         <tab :line-width="1">
-            <tab-item selected><img src="../assets/top.png" >1~50</tab-item>
-            <tab-item>51~100</tab-item>
-            <tab-item>101~150</tab-item>
-            <tab-item>151~200</tab-item>
-            <tab-item>201~250</tab-item>
+            <tab-item selected  @on-item-click="onItemClick"><img src="../assets/top.png" >1~50</tab-item>
+            <tab-item  @on-item-click="onItemClick">51~100</tab-item>
+            <tab-item  @on-item-click="onItemClick">101~150</tab-item>
+            <tab-item  @on-item-click="onItemClick">151~200</tab-item>
+            <tab-item  @on-item-click="onItemClick">201~250</tab-item>
         </tab>
         <ul>
             <li class="top-li" v-for="(film, i) in films" :key="i">
@@ -80,7 +114,18 @@ export default {
         <!-- <Divider v-show="!isLoading">已经到底部啦</Divider> -->
     </div>
 </template>
-<style>
-
+<style lang='less'>
+ul {
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: space-around;
+    align-items: center;
+    list-style: none;
+    padding: 1rem 0.7rem 0;
+    margin-bottom: 54px;
+    .top-li {
+        width: 100%;
+    }
+}
 </style>
 
